f87791ce8cc30bd88f3e733c1d744a6f5887a750
Joseph <jwgwalton@gmail.com>
2017-06-30 16:52:15 +0100

    object orientate the graph generator

--- graph_generator.py
+++ graph_generator.py
@@ -7,49 +7,52 @@ from constraints.degree_constraint import DegreeConstraint
 from constraints.node_limit_constraint import NodeLimitConstraint
 from constraints.laplacian_lambda_second_min_constraint import LaplacianLambdaSecondMinConstraint
 
-def generate_graph(n, A, constraints):
+class GraphGenerator:
 
-  np.random.seed(1)
-  M = np.random.normal(size=(n,n))
+  def __init__(self,n):
+    self.n=n
+    self.A=cvx.Symmetric(n)
+    np.random.seed(1) # move out of class?
+    self.M = np.random.normal(size=(n,n))
 
-  objective = cvx.Maximize(cvx.trace(M*A))
+  def generate_graph(self, constraints):
+    objective = cvx.Maximize(cvx.trace(self.M*self.A))
 
-  problem = cvx.Problem(objective,constraints)
+    problem = cvx.Problem(objective,constraints)
 
-  problem.solve(verbose=True)
+    problem.solve(verbose=True)
 
-  # gracefully handle failure or infeasibility?
+    # gracefully handle failure or infeasibility?
 
-  if problem.status=='optimal':
-    return problem.status,problem.value,A.value
-
-  else:
-    return problem.status,np.nan,np.nan
+    if problem.status=='optimal':
+      return problem.status,problem.value,self.A.value
+    else:
+      return problem.status,np.nan,np.nan
 
 if __name__ == '__main__':
 
   # 40 degree graph, degree *, second_smallest eigenvalue of lthe laplacian larger than 4
   n=40
 
-  # adjacency matrix of graph
-  A = cvx.Symmetric(n)
+  graph_generator = GraphGenerator(n)
 
   # all nodes have degree 8
-  degree_constraints = DegreeConstraint(n,A,8)
+  degree_constraints = DegreeConstraint(n,graph_generator.A,8)
  
   # all values between 0 and 1
-  limit_constraints = NodeLimitConstraint(A,lower_limit=0, upper_limit=1) 
+  limit_constraints = NodeLimitConstraint(graph_generator.A,lower_limit=0, upper_limit=1) 
 
   # 2nd largest eigenvalue of the laplacian >= 4
-  laplacian_constraints = LaplacianLambdaSecondMinConstraint(A,4)
+  laplacian_constraints = LaplacianLambdaSecondMinConstraint(graph_generator.A,4)
 
   # diag(A) == 0
-  diagonal_constraints = DiagonalConstraint(n,A,0) 
+  diagonal_constraints = DiagonalConstraint(n,graph_generator.A,0) 
 
   constraints = limit_constraints.constraint_list + diagonal_constraints.constraint_list + degree_constraints.constraint_list + laplacian_constraints.constraint_list
 
-  status,problem_value,graph = generate_graph(n, A, constraints)
+  status,problem_value,graph = graph_generator.generate_graph(constraints)
 
+  np.set_printoptions(suppress=True)
   print('Problem status ',status)
   print('Adjacency matrix which satisfies constraints: ', np.round(graph,5)) 
   laplacian_of_adjacency_matrix = scipy.sparse.csgraph.laplacian(graph)
