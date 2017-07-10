7a92ae6512d56d24d8d974427ed95b33c50a95f7
Joseph <jwgwalton@gmail.com>
2017-07-10 08:41:55 +0100

    removed old moved files

--- constraints/max_clique_constraint.py
+++
@@ -1,8 +0,0 @@
-from constraints.constraint import Constraint
-import numpy as np
-
-class MaxCliqueConstraint(Constraint):
-
-  def __init__(self, A, max_clique_size): 
-    self.constraint_list=[cvx.trace(A**max_clique_size)==0]
-
--- constraints/triangle_free_constraint.py
+++
@@ -1,7 +0,0 @@
-from constraints.max_clique_constraint import MaxCliqueConstraint
-import cvxpy as cvx
-
-class TriangleFreeConstraint(MaxCliqueConstraint):
-
-  def __init__(self, A): 
-    self.constraint_list=[cvx.trace(cvx.power(A,3))==0]
